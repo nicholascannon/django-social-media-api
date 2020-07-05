8cecddfb2f23cc8c8fdf2794b5c388849a0faa8b
Nicholas Cannon <nicholascannon1@gmail.com>
2020-07-05 17:01:12 +0800

    updated urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('dashboard/', admin.site.urls),
     path('auth/', include('users.urls')),
 ]
+
+handler500 = 'rest_framework.exceptions.server_error'
+handler400 = 'rest_framework.exceptions.bad_request'
