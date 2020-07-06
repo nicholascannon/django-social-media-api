7ba415ea55f563d86520999b72b78c1d5b43e684
Nicholas Cannon <nicholascannon1@gmail.com>
2020-07-06 09:24:08 +0800

    serializers update

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,30 +1,32 @@
 from rest_framework import serializers
 
 from . import models
-
-
-class PostSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        slug_field='uuid', read_only=True)
-
-    class Meta:
-        model = models.Post
-        fields = ['uuid', 'text', 'author', 'pins', 'date_created']
-        read_only_fields = ['uuid', 'pins', 'date_created']
+from users.serializers import UserDetailsSerializer
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        slug_field='username', read_only=True)
+    author = UserDetailsSerializer(read_only=True)
     post = serializers.SlugRelatedField(slug_field='uuid', read_only=True)
 
     class Meta:
         model = models.Comment
-        fields = ['uuid', 'text', 'date_created']
+        fields = ['uuid', 'text', 'date_created', 'post', 'author']
         read_only_fields = ['date_created', 'uuid']
 
 
+class PostSerializer(serializers.ModelSerializer):
+    author = UserDetailsSerializer(read_only=True)
+    comments = CommentSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = models.Post
+        fields = ['uuid', 'text', 'author', 'comments', 'pins', 'date_created']
+        read_only_fields = ['uuid', 'pins', 'date_created']
+
+
 class PostReportSerializer(serializers.ModelSerializer):
+    post = PostSerializer()
+    user = UserDetailsSerializer()
 
     class Meta:
         model = models.PostReport
