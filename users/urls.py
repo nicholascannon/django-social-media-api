a6c3f1513329caaa4659f125f7fe3c320d624d08
Nicholas Cannon <nicholascannon1@gmail.com>
2020-07-06 12:28:44 +0800

    fixed accounts urls

--- users/urls.py
+++ users/urls.py
@@ -9,10 +9,10 @@ from . import views
 
 urlpatterns = [
     path('me/', views.current_user_details, name='current_user_details'),
-    path('<str:username>/', views.user_details, name='user_details'),
     path('password/change/', views.change_password, name='change_password'),
     path('register/', views.register, name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('<str:username>/', views.user_details, name='user_details'),
 ]
