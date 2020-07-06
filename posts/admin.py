43154b62eff2e94215ee0c4afddba9853532e519
Nicholas Cannon <nicholascannon1@gmail.com>
2020-07-06 09:23:45 +0800

    added modes to admin

--- posts/admin.py
+++ posts/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 
 from . import models
 
-admin.register(models.Post)
-admin.register(models.Comment)
-admin.register(models.PostReport)
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.PostReport)
+class PostReportAdmin(admin.ModelAdmin):
+    pass
