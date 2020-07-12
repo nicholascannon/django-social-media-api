159763f2a1b29b755abeae989ad55583683efcb9
Nicholas Cannon <nicholascannon1@gmail.com>
2020-07-12 17:37:04 +0800

    added docs and comment endpoints

--- posts/urls.py
+++ posts/urls.py
@@ -3,16 +3,18 @@ from django.urls import path
 from .views import (
     PostListCreateAPIView,
     PostDetailAPIView,
-    PinPostView
+    PinPostAPIView,
+    CommentListCreateAPIView,
+    CommentDestroyAPIView
 )
 
 urlpatterns = [
     path('', PostListCreateAPIView.as_view(), name='post_list_create'),
     path('<uuid>/', PostDetailAPIView.as_view(), name='post_detail'),
-    path('<uuid>/pin/', PinPostView.as_view(), name='pin_post'),
-    # path('<uuid>/comment/', , name='post_comment'),
-    # path('<post_uuid>/comment/<comment_uuid>/',
-    #      , name='delete_comment'),
+    path('<uuid>/pin/', PinPostAPIView.as_view(), name='pin_post'),
+    path('<uuid>/comment/', CommentListCreateAPIView, name='comment_list_create'),
+    path('<post_uuid>/comment/<comment_uuid>/',
+         CommentDestroyAPIView.as_view(), name='delete_comment'),
     # path('recent/', , name='recent_posts'),
     # path('user/<uuid>/', , name='get_user_posts')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,26 @@
 from rest_framework.permissions import (
     IsAuthenticated, IsAuthenticatedOrReadOnly)
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import (
+    ListCreateAPIView, RetrieveUpdateDestroyAPIView, DestroyAPIView)
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status as s
 from django.shortcuts import get_object_or_404
 
 from .models import Post, Comment
-from .serializers import PostSerializer
+from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
 
 
 class PostListCreateAPIView(ListCreateAPIView):
+    """
+    Lists the currently logged in users posts with a GET and allows a user to 
+    create a new post with POST. Must be logged in to access this route.
+
+    EXAMPLE:
+        GET -> /posts/ -> return a list of posts
+        POST -> /posts/ -> create new post
+    """
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
 
@@ -26,6 +35,16 @@ class PostListCreateAPIView(ListCreateAPIView):
 
 
 class PostDetailAPIView(RetrieveUpdateDestroyAPIView):
+    """
+    Selects post by UUID and displays it's details. Anon users able to read post
+    details with GET. Must be authenticated and be the owner of the post to make 
+    PUT and DELETE requests.
+
+    EXAMPLE:
+        GET -> /posts/<uuid>/ -> return post details
+        PUT -> /posts/<uuid>/ -> make an edit to the post text (if owner)
+        DELETE -> /posts/<uuid>/ -> delete post (if owner)
+    """
     queryset = Post.objects.all()
     lookup_field = 'uuid'
 
@@ -36,10 +55,15 @@ class PostDetailAPIView(RetrieveUpdateDestroyAPIView):
         return serializer.save(edited=True)
 
 
-class PinPostView(APIView):
+class PinPostAPIView(APIView):
     """
-    Allows any authenticated user to pin (like) a post. Currently a user can 
-    pin the same post as many times as they like.
+    Allows any authenticated user to pin (like) a post. This endpoint increments
+    a posts pins by 1.
+
+    NOTE: Currently a user can pin the same post as many times as they like.
+
+    EXAMPLE:
+        PUT -> /posts/<uuid>/pin/ -> increment post pins by 1
     """
     permission_classes = [IsAuthenticated]
 
@@ -51,3 +75,28 @@ class PinPostView(APIView):
         post.pins += 1
         post.save()
         return Response(status=s.HTTP_200_OK)
+
+
+class CommentListCreateAPIView(ListCreateAPIView):
+    """
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        return Comment.objects.filter(post__uuid=self.kwargs['uuid'])
+
+    def perform_create(self, serializer):
+        post = Post.objects.get(uuid=self.kwargs['uuid'])
+        return serializer.save(author=self.request.user, post=post)
+
+
+class CommentDestroyAPIView(DestroyAPIView):
+    """
+    """
+    serilizer_class = CommentSerializer
+    permission_classes = [IsAuthorOrReadOnly]  # No reads on this endpoint
+
+    def get_object(self):
+        return Comment.objects.get(uuid=self.kwargs['comment_uuid'],
+                                   post_uuid=self.kwargs['post_uuid'])
